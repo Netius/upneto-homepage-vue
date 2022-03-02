5dff1e656a20958e1173a2d5c470ee5fddf700c6
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2022-03-02 18:32:15 +0100

    Email and message sendes successful

--- send_form_email.php
+++ send_form_email.php
@@ -1,35 +1,19 @@
 <?php
-if(isset($_POST['email'])) {
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "contact@upneto.com";
-    $email_subject = "Message from UPNETO Website";
-  
- 
-    $name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $message = $_POST['message']; // required
- 
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
+if(isset($_POST['submit'])) {
 
+$to      = 'contact@upneto.com';
+$subject = 'Message from upneto.com';
+$message = $_POST['message'];
+$headers = array(
+    'From' => $_POST['email'],
+    'Reply-To' => 'no-reply@upneto.com',
+    'X-Mailer' => 'PHP/' . phpversion()
+);
+
+mail($to, $subject, $message, $headers);
+}
 ?>
- 
+
 <!-- include your own success html here -->
 
 <p style="font-size: 18px; margin: 20px;">THANK YOU for contacting me! I will contact you soon.</p>
@@ -40,11 +24,3 @@ $headers = 'From: '.$email_from."\r\n".
     window.setTimeout(function() {
     window.location.href = 'https://upneto.com/index.html';}, 6000);
 </script>
-
-
-<?php
-exit();
-}
-?>
-
-
