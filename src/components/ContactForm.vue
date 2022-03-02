1caf2f002641b06338dc68b391152fbdc76660c1
NetiusSP <fabiomoraisnetosp@hotmail.com>
2022-03-02 14:00:12 +0100

    Wip form validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,3 +1,17 @@
+<script>
+  export default {
+    data() {
+      return {
+        errors: [],
+        name: null,
+        emailAddress: null,
+        message: null
+      }
+    }
+  }
+
+</script>
+
 <template>
   <section id="contact" class="container my-5">
     <div class="row d-block d-md-flex">
@@ -7,13 +21,24 @@
         <h1>Any queries <i class="bi bi-patch-question-fill fs-1"></i></h1>
       </div>
       <div class="col">
-        <form id="contactForm" action="/send_form_email.php" method="post">
+        <form id="contactForm" 
+              @submit.prevent="checkForm"
+              action="send_form_email.php" 
+              method="post">
+          <div v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+              <li v-for="error in errors">{{ error }}</li>
+            </ul>
+          </div>
           <div class="mb-3">
             <input
               class="form-control"
               id="name"
+              v-model="name"
               type="text"
               placeholder="Name"
+              required
             />
           </div>
 
@@ -21,8 +46,10 @@
             <input
               class="form-control"
               id="emailAddress"
+              v-model="emailAddress"
               type="email"
-              placeholder="Email Address"
+              placeholder="Email address"
+              required
             />
           </div>
 
@@ -30,9 +57,10 @@
             <textarea
               class="form-control"
               id="message"
+              v-model="message"
               type="text"
               placeholder="Message"
-              style="height: 10rem"
+              required
             ></textarea>
           </div>
 
@@ -40,6 +68,8 @@
             <button class="btn btn__primary btn-lg" type="submit">Send</button>
           </div>
         </form>
+        {{name}} {{message}} {{emailAddress}}
+        dasdsadas
       </div>
     </div>
   </section>
