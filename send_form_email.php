15efaee68bb4064a0b8def9ca77e3bb09d43f0b7
NetiusSP <fabiomoraisnetosp@hotmail.com>
2022-03-02 15:59:28 +0100

    Wip validation in php

--- send_form_email.php
+++ send_form_email.php
@@ -3,30 +3,11 @@ if(isset($_POST['email'])) {
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "contact@upneto.com";
     $email_subject = "Message from UPNETO Website";
- 
-    // function died($error) {
-    //     // your error code can go here
-    //     echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-    //     echo "These errors appear below.<br /><br />";
-    //     echo $error."<br /><br />";
-    //     echo "Please go back and fix these errors.<br /><br />";
-    //     die();
-    // }
- 
- 
-    // // validation expected data exists
-    // if(!isset($_POST['first_name']) ||
-    //     // !isset($_POST['last_name']) ||
-    //     !isset($_POST['email']) ||
-    //     // !isset($_POST['telephone']) ||
-    //     !isset($_POST['comments'])) {
-    //     died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    // }
- 
   
-    $first_name = $_POST['first_name']; // required
+ 
+    $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
-    $comments = $_POST['comments']; // required
+    $message = $_POST['message']; // required
  
  
     $email_message = "Form details below.\n\n";
@@ -37,9 +18,9 @@ if(isset($_POST['email'])) {
       return str_replace($bad,"",$string);
     }
  
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Name: ".clean_string($name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
@@ -56,10 +37,8 @@ $headers = 'From: '.$email_from."\r\n".
 
 <script>
   // Sendes user back to front page after submitting form
-    // window.setTimeout(function() {
-    // window.location.href = 'https://upneto.com/index.html';}, 6000);
-      
-
+    window.setTimeout(function() {
+    window.location.href = 'https://upneto.com/index.html';}, 6000);
 </script>
 
 
--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,101 +1,99 @@
 <script>
 export default {
-  data() {
-    return {
-      errors: [],
-      name: null,
-      emailAddress: null,
-      message: null
-    }
-  },
-  methods: {
-    checkForm() {
-      if (this.name && this.emailAddress && this.message) {
-        return true;
-      }
+	data() {
+		return {
+			errors: [],
+			name: null,
+			email: null,
+			message: null,
+		};
+	},
+	methods: {
+		checkForm(e) {
+		
+			this.errors = [];
 
-      this.errors = [];
-
-      if (!this.name) {
-        this.errors.push('Name required.');
-      }
-      if (!this.emailAddress) {
+			if (!this.name) {
+				this.errors.push("Name required.");
+			}
+			if (!this.email) {
         this.errors.push('Email required.');
       }
-      if (!this.message) {
-        this.errors.push('Message required.');
-      }
-    }
-  }
-}
 
+			if (!this.message) {
+				this.errors.push("Message required.");
+			}
+
+      if (!this.errors.length) {
+        this.errors=[];
+        return true;
+			}else{
+        e.preventDefault();
+      }
+
+		},
+	},
+};
 </script>
 
 <template>
-  <section
-    id="contact"
-    class="container my-5"
-  >
-    <div class="row d-block d-md-flex">
-      <div class="col secondary__bg align-self-center text-center rounded contact__queries text-dark my-3">
-        <h1>Any queries
-          <i class="bi bi-patch-question-fill fs-1"></i>
-        </h1>
-      </div>
-      <div class="col">
-        <form
-          id="contactForm"
-          @submit="checkForm"
-          action="send_form_email.php"
-          method="post"
-        >
-          <div v-if="errors.length">
-            <b>Please correct the following error(s):</b>
-            <ul>
-              <li
-                v-for="error in errors"
-                :key="error.index"
-              >{{ error }}</li>
-            </ul>
-          </div>
-          <div class="mb-3">
-            <input
-              class="form-control"
-              id="name"
-              v-model="name"
-              type="text"
-              placeholder="Name"
-            />
-          </div>
+	<section id="contact" class="container my-5">
+		<div class="row d-block d-md-flex">
+			<div
+				class="col secondary__bg align-self-center text-center rounded contact__queries text-dark my-3"
+			>
+				<h1>
+					Any queries
+					<i class="bi bi-patch-question-fill fs-1"></i>
+				</h1>
+			</div>
+			<div class="col">
+				<form
+					id="contactForm"
+					@submit="checkForm"
+					action="send_form_email.php"
+					method="post"
+				>
+					<div v-if="errors.length">
+						<ul>
+							<li class="text-danger" v-for="error in errors" :key="error.index">{{ error }}</li>
+						</ul>
+					</div>
+					<div class="mb-3">
+						<input
+							class="form-control"
+							id="name"
+							v-model="name"
+							type="text"
+							placeholder="Name"
+						/>
+					</div>
 
-          <div class="mb-3">
-            <input
-              class="form-control"
-              id="emailAddress"
-              v-model="emailAddress"
-              type="email"
-              placeholder="Email address"
-            />
-          </div>
+					<div class="mb-3">
+						<input
+							class="form-control"
+							id="email"
+							v-model="email"
+							type="email"
+							placeholder="Email address"
+						/>
+					</div>
 
-          <div class="mb-3">
-            <textarea
-              class="form-control"
-              id="message"
-              v-model="message"
-              type="text"
-              placeholder="Message"
-            ></textarea>
-          </div>
+					<div class="mb-3">
+						<textarea
+							class="form-control"
+							id="message"
+							v-model="message"
+							type="text"
+							placeholder="Message"
+						></textarea>
+					</div>
 
-          <div class="d-grid">
-            <button
-              class="btn btn__primary btn-lg"
-              type="submit"
-            >Send</button>
-          </div>
-          </form>
-      </div>
-    </div>
-    </section>
+					<div class="d-grid">
+						<button title="Send email" class="btn btn__primary btn-lg" type="submit">Send</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
 </template>
