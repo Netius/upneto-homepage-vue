4c672e3033ea9f22b114ac0be8aaf816ceb7ebfd
NetiusSP <fabiomoraisnetosp@hotmail.com>
2022-03-08 15:17:01 +0100

    Added perfil image and some text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,36 +1,25 @@
 <template>
-<div class="skillsets__container position-relative mb-5">
+	<div
+	    id="about"
+	    class="skillsets__container position-relative mb-5"
+	>
 		<div class="rotate-bg-about"></div>
 		<div class="container">
-        <h2 class="fs-2 pt-5 text-center">About me</h2>
-		
-        <ul class="row text-center list-unstyled skillsets__container--icons mt-4">
-			<li class="col-4 py-2 py-md-4">
-				<i class="fab fa-html5 fa-lg"></i>
-                <p class="mt-2">HTML 5</p>
-			</li>
-			<li class="col-4 py-2 py-md-4">
-				<i class="fab fa-js fa-lg"></i>
-                <p class="mt-2">JavaScript</p>
-			</li>
-			<li class="col-4 py-2 py-md-4">
-				<i class="fab fa-react fa-lg"></i>
-                <p class="mt-2">React</p>
-			</li>
+			<h2 class="fs-2 pt-2 pt-md-5 text-center text-white">About me</h2>
 
-			<li class="col-4 py-2 py-md-4">
-				<i class="fab fa-css3-alt fa-lg"></i>
-                <p class="mt-2">CSS 3</p>
-			</li>
-			<li class="col-4 py-2 py-md-4">
-				<i class="fab fa-vuejs fa-lg"></i>
-                <p class="mt-2">VueJs</p>
-			</li>
-			<li class="col-4 py-2 py-md-4">
-				<i class="fab fa-sass fa-lg"></i>
-                <p class="mt-2">Sass</p>
-			</li>
-		</ul>
-        </div>
-	</div>
+			<div class="row mt-4">
+				<div class="col-md-4 col-sm-12 text-center p-0">
+					<img
+					    src="../assets/foto.png"
+					    title="Fabio Neto"
+					    class="w-50 img-thumbnail rounded about__image"
+					    alt="Fabio Neto image"
+					>
+				</div>
+				<div class="col-md-8 col-sm-12 pt-3 pt-md-1">
+					<p class="text-white fs-4 about__text">I am a Front-end Developer student at Noroff University in Oslo, Norway (started since march, 2019). <br> Always loved to build things and maybe we can build your next Web application together.</p>
+				</div>
+			</div>
+		</div>
+		</div>
 </template>
