93724b970f50001556ba2bded2bb32705611b887
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-02-07 15:41:06 +0100

    Wip on adding prj to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,33 +4,19 @@
 
 		<div class="card-group">
 			<div class="card m-3">
-				<img
-					src="../assets/upneto-site.jpg"
-					class="img-fluid rounded-start"
-					alt="Upneto"
-				/>
+				<img src="../assets/upneto-site.jpg" class="img-fluid rounded-start" alt="Upneto" />
 				<div class="card-body">
-					<h3 class="card-title fs-4 fs-4">UPNETO</h3>
+					<h3 class="card-title fs-4 fs-4">Portfolio 2 - UPNETO</h3>
 					<p class="card-text pb-4">
 						My own portfolio website create in Vue3 Cli with contact form using
 						PHP on the server side.
 					</p>
-					<a
-						title="Visit website"
-						class="btn btn__primary me-3 mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://upneto.com"
-						>Website
+					<a title="Visit website" class="btn btn__primary me-3 mb-2" target="_blank" rel="noreferer"
+						href="https://upneto.com">Website
 					</a>
 
-					<a
-						title="See repository"
-						class="btn btn__outline--primary mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://github.com/Netius/upneto-homepage-vue"
-						>Github
+					<a title="See repository" class="btn btn__outline--primary mb-2" target="_blank" rel="noreferer"
+						href="https://github.com/Netius/upneto-homepage-vue">Github
 					</a>
 					<p class="card-text">
 						<small class="text-muted">Project finished: 13.03.2022</small>
@@ -39,35 +25,21 @@
 			</div>
 
 			<div class="card m-3">
-				<img
-					src="../assets/dogshop.jpg"
-					class="img-fluid rounded-start"
-					alt="Dogshop"
-				/>
+				<img src="../assets/dogshop.jpg" class="img-fluid rounded-start" alt="Dogshop" />
 				<div class="card-body">
-					<h3 class="card-title fs-4 fs-4">DogShop</h3>
+					<h3 class="card-title fs-4 fs-4">Semester Project 2 - DogShop</h3>
 					<p class="card-text pb-4">
 						Semester project done in plain Javascript with administrasjon
 						interface to remove and add more products.<br />
 						Deployed in Strapi api on Heroku using Cloudinary as third party CMS
 						for images.
 					</p>
-					<a
-						title="Visit website"
-						class="btn btn__primary me-3 mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://upneto.com/htmlcss/dogshop/index.html"
-						>Website
+					<a title="Visit website" class="btn btn__primary me-3 mb-2" target="_blank" rel="noreferer"
+						href="https://upneto.com/htmlcss/dogshop/index.html">Website
 					</a>
 
-					<a
-						title="See repository"
-						class="btn btn__outline--primary mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://github.com/Netius/DogShop"
-						>Github
+					<a title="See repository" class="btn btn__outline--primary mb-2" target="_blank" rel="noreferer"
+						href="https://github.com/Netius/DogShop">Github
 					</a>
 					<p class="card-text">
 						<small class="text-muted">Project finished: 16.01.2022</small>
@@ -75,13 +47,69 @@
 				</div>
 			</div>
 		</div>
+
+
+
+
+
+
+
 		<div class="card-group">
 			<div class="card m-3">
-				<img
-					src="../assets/react-appointments.jpg"
-					class="img-fluid rounded-start"
-					alt="React appointments"
-				/>
+				<img src="../assets/js-famework-ca-hero.png " class="img-fluid rounded-start"
+					alt="JavaScript Frameworks CA" />
+				<div class="card-body">
+					<h3 class="card-title fs-4 fs-4">JavaScript Frameworks CA</h3>
+					<p class="card-text pb-4">
+						Course assignment bootstrapped with Create React App and using WordPress site as Rest API to
+						retrieve data and security login.
+					</p>
+					<a title="Visit website" class="btn btn__primary me-3 mb-2" target="_blank" rel="noreferer"
+						href="https://upneto.com/htmlcss/js-frameworks-ca/index.html">Website
+					</a>
+
+					<a title="See repository" class="btn btn__outline--primary mb-2" target="_blank" rel="noreferer"
+						href="https://github.com/Noroff-FEU-Assignments/js-frameworks-course-assignment-Netius">Github
+					</a>
+
+					<p class="card-text">
+						<small class="text-muted">Project finished: 08.10.2022</small>
+					</p>
+				</div>
+			</div>
+
+			<div class="card m-3">
+				<img src="../assets/holidaze-exam-2.jpg " class="img-fluid rounded-start" alt="Holidaze" />
+				<div class="card-body">
+					<h3 class="card-title fs-4 fs-4">Project Exam 2 - Holidaze</h3>
+					<p class="card-text pb-4">
+						xxx
+					</p>
+					<a title="Visit website" class="btn btn__primary me-3 mb-2" target="_blank" rel="noreferer"
+						href="https://upneto.com/htmlcss/holidaze/index.html">Website
+					</a>
+
+					<a title="See repository" class="btn btn__outline--primary mb-2" target="_blank" rel="noreferer"
+						href="https://github.com/Netius/holidaze-app">Github
+					</a>
+					<p class="card-text">
+						<small class="text-muted">Project finished: 15.01.2023</small>
+					</p>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+		<div class="card-group">
+			<div class="card m-3">
+				<img src="../assets/react-appointments.jpg" class="img-fluid rounded-start" alt="React appointments" />
 				<div class="card-body">
 					<h3 class="card-title fs-4 fs-4">React Appointments</h3>
 					<p class="card-text pb-4">
@@ -89,22 +117,12 @@
 						and toggle items by passing the state to a sub-component and using
 						values to hide or show sub-components. (by inLearning)
 					</p>
-					<a
-						title="Visit website"
-						class="btn btn__primary me-3 mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://upneto.com/htmlcss/react-appointments/index.html"
-						>Website
+					<a title="Visit website" class="btn btn__primary me-3 mb-2" target="_blank" rel="noreferer"
+						href="https://upneto.com/htmlcss/react-appointments/index.html">Website
 					</a>
 
-					<a
-						title="See repository"
-						class="btn btn__outline--primary mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://github.com/Netius/reactinterface2"
-						>Github
+					<a title="See repository" class="btn btn__outline--primary mb-2" target="_blank" rel="noreferer"
+						href="https://github.com/Netius/reactinterface2">Github
 					</a>
 
 					<p class="card-text">
@@ -114,11 +132,7 @@
 			</div>
 
 			<div class="card m-3">
-				<img
-					src="../assets/ammerudbasket.jpg"
-					class="img-fluid rounded-start"
-					alt="Ammerud Basket"
-				/>
+				<img src="../assets/ammerudbasket.jpg" class="img-fluid rounded-start" alt="Ammerud Basket" />
 				<div class="card-body">
 					<h3 class="card-title fs-4 fs-4">Ammerud Basket</h3>
 					<p class="card-text pb-4">
@@ -126,13 +140,8 @@
 						Yootheme builder to create a simple and effective website for
 						Ammerud Basket.
 					</p>
-					<a
-						title="Visit website"
-						class="btn btn__primary me-3 mb-2"
-						target="_blank"
-						rel="noreferer"
-						href="https://www.ammerudbasket.no/"
-						>Website
+					<a title="Visit website" class="btn btn__primary me-3 mb-2" target="_blank" rel="noreferer"
+						href="https://www.ammerudbasket.no/">Website
 					</a>
 					<p class="card-text">
 						<small class="text-muted">Project finished: still going</small>
