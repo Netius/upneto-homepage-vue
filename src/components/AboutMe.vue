2da741519b44879c8af8a29b47e24676508e341f
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2024-07-16 21:12:38 +0200

    Fixed style and some more updated tex

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,25 +1,24 @@
 <template>
-	<div
-	    id="about"
-	    class="skillsets__container position-relative mb-5"
-	>
+	<div id="about" class="skillsets__container position-relative mb-5">
 		<div class="rotate-bg-about"></div>
 		<div class="container">
 			<h2 class="fs-2 pt-5 pt-md-5 text-center text-white">About me</h2>
 
 			<div class="row mt-4">
 				<div class="col-md-4 col-sm-12 text-center p-0">
-					<img
-					    src="../assets/foto.jpg"
-					    title="Fabio Neto"
-					    class="w-50 img-thumbnail rounded about__image"
-					    alt="Fabio Neto image"
-					>
+					<img src="../assets/foto.jpg" title="Fabio Neto" class="img-thumbnail rounded about__image"
+						alt="Fabio Neto image">
 				</div>
 				<div class="col-md-8 col-sm-12 pt-3 pt-md-1">
-					<p class="text-white fs-4 about__text">I am a Front-end Developer student at Noroff University in Oslo, Norway (started since march, 2019). <br> Always loved to build things and maybe we can build your next Web application together.</p>
+					<p class="text-white fs-4 about__text">
+						Front-end developer with 4+ years of experience in React, Vue 3, Angular, JavaScript / Typescript, HTML5 and
+						CSS3.
+					</p>
+					<p class="text-white fs-4 about__text">
+						Experience also with C#, .NET, Blazor, GraphQL, Docker, Node.js.
+					</p>
 				</div>
 			</div>
 		</div>
-		</div>
+	</div>
 </template>
